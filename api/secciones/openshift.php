004b4387bf2d09a13fdb018c76026b1f10772d4d
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:28:30 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Ya no se me ocurre nada",
+"autor" => "Deybi Chávez",
+"resumen" => "No se que puedo poner aqui, ya muchas noticias",
 ];
 ?>
