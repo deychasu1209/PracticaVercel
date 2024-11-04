5e7d6e7d3000e087624eaff34f437bf0608438d6
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:27:37 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Dinero no declarado de la presidenta Dina Boluarte podría llevarla a un proceso de vacancia",
+"autor" => "Anónimo",
+"resumen" => "La presidenta Dina Boluarte enfrenta una creciente presión política y social debido a la revelación de la existencia de dinero no declarado en su entorno, lo que ha generado demandas de vacancia en su contra. Este nuevo escándalo se suma a otras controversias que han marcado su gestión, incluyendo la investigación por la posesión de relojes Rolex, considerados un indicio de enriquecimiento ilícito. La situación ha polarizado a la opinión pública, y líderes de diferentes sectores han exigido transparencia y rendición de cuentas del gobierno.",
 ];
 ?>
