63ac0a13aeafa4deb60282d51ab2ec7b7c25bdff
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:33:05 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El desarrollo de chips consume más energía que algunos países",
+"autor" => "Luis Enrique Velasco",
+"resumen" => "Los expertos advierten que la creciente demanda de microprocesadores en el mercado ha llevado a un consumo energético alarmante en la fabricación de chips, comparable al de naciones enteras. Este problema se presenta como un obstáculo para la transición hacia energías más sostenibles, lo que plantea interrogantes sobre la viabilidad ambiental de satisfacer la demanda tecnológica actual​.",
 ];
 ?>
