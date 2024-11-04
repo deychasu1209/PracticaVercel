3f304c50b87c7e5a940c183e39ee738a287424af
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:20:23 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Billie Eilish insta a sus fans a votar por Kamala Harris en concierto en Atlanta",
+"autor" => "Sara Delgado",
+"resumen" => "Durante su concierto en Atlanta, Billie Eilish interrumpió su actuación para motivar a sus seguidores a votar por Kamala Harris, a solo dos días de las elecciones presidenciales de EE. UU. Eilish subrayó la importancia de la situación actual, instando a los asistentes a apoyar a Harris, quien, según ella, defiende los derechos reproductivos, el medio ambiente y la democracia. Eilish y su hermano, Finneas, han respaldado públicamente a Harris en redes sociales desde septiembre, enfatizando la necesidad de que los jóvenes voten como si sus vidas dependieran de ello.",
 ];
 ?>
