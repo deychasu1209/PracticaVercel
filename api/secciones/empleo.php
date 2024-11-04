30551bc552bdeda7a74dd172e93d1db1c8be30d3
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:23:33 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El Mercado Laboral de EE.UU. Crece a un Ritmo Sostenido en Octubre de 2024",
+"autor" => "Oficina de Estadísticas Laborales de EE.UU.",
+"resumen" => "En octubre de 2024, el empleo en EE.UU. mostró un ligero aumento con 182,000 nuevos puestos de trabajo añadidos, superando las expectativas del mercado. Las industrias de servicios, especialmente en atención médica y asistencia social, experimentaron un crecimiento notable. Sin embargo, el sector manufacturero vio una leve disminución en los empleos, reflejando un panorama mixto en diferentes sectores. A pesar de estos desafíos, los salarios promedio continuaron aumentando, con un incremento del 4.5% en el último año, lo que indica una presión inflacionaria persistente en el mercado laboral​.",
 ];
 ?>
