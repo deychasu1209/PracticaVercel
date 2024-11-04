9dca7b5c1f6a751de57ff9354fd11fee46e94323
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:33:31 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "RTVE renueva su Consejo de Administración ante críticas y cambios legislativos",
+"autor" => "Redacción RTVE",
+"resumen" => "El Gobierno español ha aprobado un real decreto para renovar urgentemente el Consejo de Administración de RTVE, que debe completarse antes del 23 de noviembre. Este cambio modifica las mayorías parlamentarias requeridas para la elección de consejeros, ampliando el número de miembros de 10 a 15. El nuevo consejo, que será elegido en los próximos días, busca adaptarse a la situación actual de la corporación y se espera que sea más plural. Sin embargo, ha suscitado críticas de varios sectores que consideran que podría aumentar el control del gobierno sobre la televisión pública​.",
 ];
 ?>
