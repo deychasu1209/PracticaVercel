f41cd3daee84128e3c4e04b569b825db724c8d5d
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:22:43 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El crecimiento de la economía de EE. UU. se ralentiza mientras la inflación muestra signos de estabilidad",
+"autor" => "Anónimo",
+"resumen" => "La economía estadounidense está experimentando un crecimiento más lento, con un aumento del Producto Interno Bruto (PIB) de solo un 2.2% anual en el tercer trimestre de 2024. A pesar de esto, la inflación parece estabilizarse, con una tasa del 3.4%, lo que ha llevado a la Reserva Federal a reevaluar sus políticas monetarias. En septiembre, el ingreso personal aumentó un 0.3%, mientras que los gastos de consumo crecieron un 0.5%, indicando que los consumidores siguen activos a pesar de las tensiones económicas. Además, el déficit en la cuenta corriente de EE. UU. se amplió a 266.8 mil millones de dólares, lo que refleja preocupaciones sobre la balanza comercial del país",
 ];
 ?>
