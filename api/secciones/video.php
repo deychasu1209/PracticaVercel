406a456660048ce4c8c59a167380d64a831bdf26
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:34:19 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -6,7 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $video = [
 "titulo" => "México alista a sus mejores jóvenes para la Olimpiada Mundial de Robótica 2024 en Turquía",
 "autor" => "WIRED",
-"resumen" => "Equipos mexicanos de robótica han sido seleccionados para representar a México en la Olimpiada Mundial de Robótica (WRO) 2024 en Turquía. Los equipos RoboTecnica KND y Tamalitos, tras destacarse en la eliminatoria nacional, buscarán demostrar su talento en este importante evento. Sin embargo, enfrentan el desafío de reunir los fondos necesarios para cubrir los costos de viaje, alojamiento y alimentación, que ascienden a 420,000 pesos mexicanos. Se están llevando a cabo campañas de recaudación para ayudar a estos jóvenes a llevar el nombre de México al escenario internacional​
-",
+"resumen" => "Equipos mexicanos de robótica han sido seleccionados para representar a México en la Olimpiada Mundial de Robótica (WRO) 2024 en Turquía. Los equipos RoboTecnica KND y Tamalitos, tras destacarse en la eliminatoria nacional, buscarán demostrar su talento en este importante evento. Sin embargo, enfrentan el desafío de reunir los fondos necesarios para cubrir los costos de viaje, alojamiento y alimentación, que ascienden a 420,000 pesos mexicanos. Se están llevando a cabo campañas de recaudación para ayudar a estos jóvenes a llevar el nombre de México al escenario internacional​.",
 ];
 ?>
