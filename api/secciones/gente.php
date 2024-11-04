8f1627f8fd320dfc34aa218272f7010f008b1e07
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:25:16 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Al Pacino promete revelar su verdadero yo en un nuevo libro de memorias.",
+"autor" => "Anónimo",
+"resumen" => "En su nuevo libro, Al Pacino se propone mostrar su verdadera esencia más allá de la imagen pública que ha construido como actor. Aunque promete una mirada profunda y personal, se sugiere que puede no desnudarse completamente en su narrativa, dejando a los lectores con la intriga sobre los detalles más íntimos de su vida y carrera.",
 ];
 ?>
