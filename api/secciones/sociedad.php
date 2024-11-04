a1ddd88b1571118ad93e46131640c35277582c5f
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:31:54 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La inclusión escolar de niños con discapacidad: un debate en La Pampa",
+"autor" => "Redacción de Infobae",
+"resumen" => "En La Pampa, se ha implementado un sistema que obliga a todos los alumnos neurodiversos o con discapacidades a asistir a escuelas comunes, lo que ha generado preocupación entre los padres. Muchos de ellos han comenzado a organizarse para reclamar la reapertura de escuelas especializadas, argumentando que este enfoque puede no ser el más adecuado para todos los niños. Este debate sobre la inclusión y la adecuación de los entornos educativos sigue siendo un tema candente en la sociedad argentina, resaltando las diferentes opiniones sobre la mejor manera de apoyar a estos estudiantes.",
 ];
 ?>
