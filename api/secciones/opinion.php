d69b597627f433bcb16f6fc72f51cc00d7c422af
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:29:57 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La “generación de cristal”",
+"autor" => "Juan Gabriel Vásquez",
+"resumen" => "En un contexto donde las críticas hacia los jóvenes de la “generación de cristal” se han vuelto comunes, el autor argumenta que esta percepción es simplista y no considera las complejidades de una juventud que enfrenta desafíos significativos. Los jóvenes son retratados como frágiles y fácilmente ofendidos, pero esta visión ignora sus luchas contra problemas estructurales como la desigualdad y la violencia. El artículo invita a reflexionar sobre cómo se debe abordar la narrativa acerca de las nuevas generaciones, enfatizando la necesidad de un enfoque más comprensivo y menos despectivo hacia su activismo y sensibilidad.",
 ];
 ?>
