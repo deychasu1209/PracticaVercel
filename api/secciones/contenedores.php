6f6da2dd982e39a7043e5d6f6232cdf1c025f97d
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:21:24 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Maersk incrementa su previsión de ganancias para 2024",
+"autor" => "Greg Knowler, Editor Senior",
+"resumen" => "Maersk ha aumentado su previsión de ganancias para el año 2024, señalando un rendimiento financiero más fuerte de lo esperado en el tercer trimestre. Esta es la segunda vez que la compañía revisa al alza sus proyecciones desde agosto, reflejando una recuperación continua en el sector de la logística marítima y el transporte de contenedores. La compañía está experimentando una sólida demanda, especialmente en el tráfico transatlántico y en rutas hacia Asia, impulsando así su optimismo sobre la rentabilidad en el futuro cercano​.",
 ];
 ?>
