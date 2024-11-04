3f8f048d5cd8a320ec65c21b13b3d72ff94e4460
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:27:01 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Las elecciones de 2024: un desafío para la democracia mundial",
+"autor" => "Anónimo",
+"resumen" => "En 2024, más de 4 mil millones de personas votarán en 76 países, enfrentando un panorama electoral complejo. Mientras algunos votarán en democracias plenas, otros lo harán en regímenes híbridos o autoritarios, como Rusia e Irán. La participación de jóvenes y mujeres será crucial, especialmente en contextos como el de EE. UU., donde el miedo a la violencia electoral está aumentando. Este ciclo electoral podría definir el futuro de las democracias, ante la creciente fragmentación y el extremismo político.",
 ];
 ?>
