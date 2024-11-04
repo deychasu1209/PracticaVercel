06d24f32c10520e5f77fcf01d7e122c94b3feb2b
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:24:43 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+"titulo" => "La Formación Liderada por Empleados Gana Terreno en 2024",
+"autor" => "Sam Zheng",
+"resumen" => "En 2024, se espera que la formación liderada por empleados transforme el panorama de Aprendizaje y Desarrollo (L&D) al poner a los empleados en el centro de su proceso de aprendizaje. Este enfoque, que se aleja de los métodos tradicionales de formación jerárquica, permite a los trabajadores elegir qué, cómo y cuándo aprender, lo que aumenta su compromiso y adaptabilidad ante los cambios en sus roles. Además, el aprendizaje entre pares se está volviendo fundamental, ya que los empleados comparten sus conocimientos y experiencias, creando un entorno de aprendizaje colaborativo. La integración de herramientas de inteligencia artificial también está mejorando la accesibilidad y personalización de la formación, lo que es crucial en un mercado laboral donde la demanda de habilidades específicas está en aumento.",
 ];
 ?>
