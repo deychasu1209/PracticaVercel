45de4bc56b86dfab8d3f90dcc4bdb1bb9f0c0088
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:18:45 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Crean un sistema de detección de desastres que usa inteligencia artificial",
+"autor" => "Karen Roberts",
+"resumen" => "Investigadores de la Universidad de Tokio han desarrollado un innovador sistema de detección de desastres que utiliza inteligencia artificial para anticipar terremotos y otros eventos sísmicos. Este sistema analiza datos en tiempo real, como movimientos del suelo y cambios en la presión atmosférica, para predecir la ocurrencia de desastres con mayor precisión. Su implementación podría ayudar a salvar vidas al proporcionar alertas tempranas, permitiendo a las comunidades prepararse mejor para enfrentar estos eventos. Además, se están realizando pruebas en diversas regiones propensas a desastres para evaluar su efectividad y fiabilidad en diferentes condiciones geológicas.",
 ];
 ?>
