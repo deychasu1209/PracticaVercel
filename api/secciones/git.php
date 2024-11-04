be261eb2ee67b71299ff01dd4522fcf3e7a9de67
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-04 10:26:18 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Git 2.43: Novedades y mejoras en la gestión de objetos",
+"autor" => "Taylor Blau",
+"resumen" => "La reciente versión de Git, 2.43, ha sido lanzada con mejoras significativas que optimizan la gestión de objetos en los repositorios. Una de las actualizaciones más destacadas es la capacidad de git repack para manejar múltiples paquetes "cruft", lo que permite dividir objetos inalcanzables en varios paquetes en lugar de mantenerlos todos en uno solo. Esto mejora el rendimiento al evitar la sobrecarga de reescribir grandes paquetes cada vez que se realiza una recolección de basura. Además, se han introducido opciones como --max-cruft-size para limitar el tamaño de cada paquete, facilitando así una gestión más eficiente de los objetos en los repositorios. Estas mejoras son parte de un esfuerzo continuo para hacer de Git una herramienta aún más robusta para los desarrolladores.",
 ];
 ?>
